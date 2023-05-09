210fd5d839519400f7a4264309d488280723c510
cahdiourhim <chadillusion@gmail.com>
2023-05-10 01:22:54 +0200

    git init message

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Link } from "react-router-dom";
+
+
+import Connexion from "./pages/Connexion";
+
+
+/*import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+
+  {
+    path: "CONNEXION",
+    element: <Connexion/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+    <RouterProvider router={router} />
+
+);*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
